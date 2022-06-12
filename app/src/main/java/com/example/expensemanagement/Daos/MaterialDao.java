c0a65fc97368eaa14d75001e2e620652e951681f
tariqHajeer <tareqhajer@gmail.com>
2022-06-12 04:21:37 +0300

    add seeder

--- app/src/main/java/com/example/expensemanagement/Daos/MaterialDao.java
+++ app/src/main/java/com/example/expensemanagement/Daos/MaterialDao.java
@@ -15,14 +15,14 @@ import java.util.List;
 @Dao
 public interface MaterialDao {
     @Insert
-    void Insert(Material material);
+    void insert(Material material);
 
     @Delete
-    void Delete(Material material);
+    void delete(Material material);
 
     @Update
-    void Update(Material material);
+    void update(Material material);
 
     @Query("Select * from Material")
-    LiveData<List<Material>> GetAll();
+    LiveData<List<Material>> getAll();
 }
--- app/src/main/java/com/example/expensemanagement/Database/ExpenseManagementDatabase.java
+++ app/src/main/java/com/example/expensemanagement/Database/ExpenseManagementDatabase.java
@@ -1,14 +1,19 @@
 package com.example.expensemanagement.Database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.expensemanagement.Daos.MaterialDao;
 import com.example.expensemanagement.Domain.Material;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,6 +21,7 @@ import java.util.concurrent.Executors;
 public abstract class ExpenseManagementDatabase extends RoomDatabase {
     public abstract MaterialDao materialDao();
 
+    private static final String dbName = "ExpenseManagementDatabase";
     private static volatile ExpenseManagementDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
     static final ExecutorService databaseWriteExecutor =
@@ -26,11 +32,39 @@ public abstract class ExpenseManagementDatabase extends RoomDatabase {
             synchronized (ExpenseManagementDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            ExpenseManagementDatabase.class, "word_database")
+                            ExpenseManagementDatabase.class, dbName)
+                            .fallbackToDestructiveMigration()
+                            .addCallback(callback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback callback = new Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new SeedMaterialAsyncTask(INSTANCE.materialDao()).execute();
+        }
+    };
+
+    private static class SeedMaterialAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private MaterialDao materialDao;
+
+        public SeedMaterialAsyncTask(MaterialDao materialDao) {
+            this.materialDao = materialDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Material electricityBill = new Material("Electricity bill", null, true);
+            materialDao.insert(electricityBill);
+            Material waterBill = new Material("Water bill", null, true);
+            materialDao.insert(waterBill);
+            return null;
+        }
     }
 }
