bf3cb23a8285bc0da6ae4062b1bd45b14c147c93
tariqHajeer <tareqhajer@gmail.com>
2022-06-16 18:28:37 +0300

    fix login main activity

--- app/src/main/java/com/example/expensemanagement/MainActivity.java
+++ app/src/main/java/com/example/expensemanagement/MainActivity.java
@@ -32,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             i = new Intent(this, Login.class);
         }
-        i = new Intent(this, DrawerActivity.class);
-        finish();
+        
         startActivity(i);
 
     }
